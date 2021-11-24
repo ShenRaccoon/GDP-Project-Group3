2fe001761b7905719d5dd3e9da1f29cfaeb8b59d
boredog <93973521+boredog@users.noreply.github.com>
2021-11-25 05:01:21 +0800

    Update TrashSend.cs

--- TrashSend.cs
+++ TrashSend.cs
@@ -69,9 +69,6 @@ public class TrashSend : MonoBehaviour, IDropHandler
                            }
                        }
                    } */
-                
-                
-                
             }
         }
     }
